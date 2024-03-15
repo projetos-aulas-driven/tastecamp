ba4cb8e8a5d7a0bb68870e0fae5d57dbb71dc14d
leticia-chijo <leticia.chijo@gmail.com>
2024-03-15 18:26:18 -0300

    Aula - axios.get()

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-const receitas = [
-    { titulo: "Lasanha", ingredientes: "Massa, molho, recheio", preparo: "Coloque camadas em uam forma e asse por 30 minutos" },
-    { titulo: "Strogonoff", ingredientes: "Carne, creme de leite, mostarda", preparo: "Refogue tudo em uma panela" },
-    { titulo: "Pão", ingredientes: "Farinha, ovos, fermento", preparo: "Misture tudo, deixe descansar e asse" }
-];
+let receitas = [];
 
 function renderizarReceitas() {
     const ul = document.querySelector(".receitas");
@@ -18,35 +14,34 @@ function renderizarReceitas() {
     }
 }
 
-renderizarReceitas();
+function buscarReceitas() {
+    const promessa = axios.get("https://mock-api.driven.com.br/api/v2/tastecamp/receitas");
+    promessa.then(processarListaRecebida);
+    promessa.catch(mostrarErro);
+}
+
+function processarListaRecebida(resposta) {
+    console.log(resposta);
+    receitas = resposta.data;
+    renderizarReceitas();
+}
 
 function adicionarReceita() {
-    // Pegar os inputs e os textareas => texto, value
     const campoTitulo = document.querySelector(".nome-receita");
     const campoIngredientes = document.querySelector(".ingredientes-receita");
     const campoPreparo = document.querySelector(".modo-preparo-receita");
 
-    // Organizar os valores em um objeto
     const novaReceita = {
         titulo: campoTitulo.value,
         ingredientes: campoIngredientes.value,
         preparo: campoPreparo.value
     }
 
-    // 1 - Enviar a receita para o servidor
     const promessa = axios.post("https://mock-api.driven.com.br/api/v2/tastecamp/receitas", novaReceita);
-
-    // 2 - Receber a resposta e ver como ela é
     promessa.then(receberResposta);
-
-    // 4 - Executar uma função caso dê erro
     promessa.catch(mostrarErro);
-
-    // Renderizar isso na tela
-    renderizarReceitas();
 }
 
-// 3 - Processar essa resposta
 function receberResposta(resposta) {
     console.log(resposta);
 
@@ -55,8 +50,13 @@ function receberResposta(resposta) {
     document.querySelector(".nome-receita").value = "";
     document.querySelector(".ingredientes-receita").value = "";
     document.querySelector(".modo-preparo-receita").value = "";
+
+    buscarReceitas();
 }
 
 function mostrarErro() {
     alert("Ocorreu um erro, tente novamente mais tarde!");
 }
+
+// Funções que são executadas ao abrir o site
+buscarReceitas();
